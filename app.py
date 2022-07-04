f615f5cca87b8a9fe19c940712c8b76223923563
k-kurumi <optpia.kurumi@gmail.com>
2022-07-05 06:34:23 +0900

    add publication_date, publication_date to format

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import csv
 import os
 import sys
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Dict, List, TextIO
 from xml.etree import ElementTree
 
@@ -16,6 +17,8 @@ class Book:
     title_yomi: str
     authors: List[str]
     publishers: List[str]
+    publication_date: str
+    purchase_date: str
 
     @property
     def row_dict(self) -> Dict:
@@ -25,12 +28,22 @@ class Book:
             "title_yomi": self.title_yomi,
             "authors": ", ".join(self.authors),
             "publishers": ", ".join(self.publishers),
+            "publication_date": self.publication_date,
+            "purchase_date": self.purchase_date,
         }
 
     @staticmethod
     def csv_fieldnames() -> List[str]:
         """csvのヘッダ部分"""
-        return ["asin", "title", "title_yomi", "authors", "publishers"]
+        return [
+            "asin",
+            "title",
+            "title_yomi",
+            "authors",
+            "publishers",
+            "publication_date",
+            "purchase_date",
+        ]
 
 
 def export_csv(books: List[Book], output: TextIO) -> None:
@@ -44,6 +57,21 @@ def export_csv(books: List[Book], output: TextIO) -> None:
 
     for b in books:
         writer.writerow(b.row_dict)
+
+
+def datetime_to_date(dt: str) -> str:
+    """datetimeに変換可能な文字から日付だけを返す
+    datetime.fromisoformatはtimezoneの表記揺れに対応していないため書き換えて処理する
+    https://qiita.com/kg1/items/c455cb643c9a12096ab1#python-37%E3%81%AEfromisoformat%E3%81%AE%E5%88%A9%E7%94%A8%E3%81%AF%E6%B3%A8%E6%84%8F
+    """
+    if dt is None or len(dt) == 0:
+        # 出版日が入っていないものが結講ある
+        return ""
+
+    # 末尾 0000 を 00:00 に書き換え
+    dt_ = f"{dt[:-2]}:{dt[-2:]}"
+    t = datetime.fromisoformat(dt_)
+    return t.strftime("%Y-%m-%d")
 
 
 @click.command()
@@ -73,6 +101,7 @@ def main(i: TextIO, o: TextIO) -> None:
             continue
 
         # よみはひらがなの方が分かりやすい
+        # よみが空文字の書籍がある
         title_yomi = jaconv.kata2hira(
             book.find("title").attrib.get("pronunciation", "")
         )
@@ -81,6 +110,12 @@ def main(i: TextIO, o: TextIO) -> None:
         authors = [a.text for a in book.find("authors")]
         publishers = [p.text for p in book.find("publishers")]
 
+        # 出版日(出版日が空文字の書籍がある)
+        publication_date = datetime_to_date(book.find("publication_date").text)
+
+        # 購入日
+        purchase_date = datetime_to_date(book.find("purchase_date").text)
+
         books.append(
             Book(
                 asin=asin,
@@ -88,6 +123,8 @@ def main(i: TextIO, o: TextIO) -> None:
                 title_yomi=title_yomi,
                 authors=authors,
                 publishers=publishers,
+                publication_date=publication_date,
+                purchase_date=purchase_date,
             )
         )
 
